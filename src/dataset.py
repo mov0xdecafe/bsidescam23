a81105517a4908790c6b643e6fb17f09cbbbc268
Nathan <Nathan.Rutherford@protonmail.com>
2023-08-29 08:18:53 +0100

    start reading fies

--- src/dataset.py
+++ src/dataset.py
@@ -1,13 +1,33 @@
-class DatasetGenerator():
+import os
+
+import lief
+import lief.logging
+import numpy
+from torch.utils import data
+from tqdm import tqdm
+
+lief.logging.set_level(lief.logging.LOGGING_LEVEL.CRITICAL)
+
+class FunctionIdentificationDataset(data.Dataset):
     """Generates the dataset for processing by the CNN"""
+    def __init__(self, root):
+        self._preprocess(root)
+        
+        
+    def _preprocess(self, root):
+        bins_paths = []
+        bins_data = []
+        bins_tags = []
 
-    def __init__(self):
-        pass
+        all_files = [os.path.join(dirpath, file) 
+            for dirpath, _, filenames in os.walk(root) 
+            for file in filenames]
 
-    def _get_code_elf(self, elf_bin=None):
-        if elf_bin is None:
-            return None
+        for file_path in tqdm(all_files):
+            bin = lief.parse(file_path)
+            if not bin:
+                continue
+            data = self._get_code(bin)
 
-class FunctionIdentificationDataset(torch.utils.data.Dataset):
-    def __init__(self):
-        pass
+    def _get_code(self, bin):
+        return numpy.array(bin.get_section(".text").content, dtype=int)
--- src/driver.py
+++ src/driver.py
@@ -1,4 +1,12 @@
-#/usr/bin/env python3
+import argparse
+
+from dataset import FunctionIdentificationDataset
 
 if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("dataset_path", help="Path to directory contain binaries to include in dataset")
+
+    args = arg_parser.parse_args()
+
     print("Function Boundary Identification")
+    dataset = FunctionIdentificationDataset(args.dataset_path)
