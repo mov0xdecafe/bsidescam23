6a9b50c44fd634c21eb63eca07b09b6b6be031dc
Nathan <Nathan.Rutherford@protonmail.com>
2023-08-29 09:30:12 +0000

    update return features vector

--- src/dataset.py
+++ src/dataset.py
@@ -41,12 +41,29 @@ class FunctionIdentificationDataset(data.Dataset):
     def _generate_features(self, bin: lief._lief.ELF.Binary):
         """Generate a list of features for the dataset"""
         text_section = bin.get_section(".text")
+        
+        # list of symbols that are not commonly stored in the .text section
+        excluded_symbols = ["_fini", "_init"] 
 
         # list of function relative start addresses 
         start_addrs = []
+
+        # calculate the relative start address / offset of all function symbols in the binary
         for symbol in bin.symbols:
+            if symbol.name in excluded_symbols:
+                continue
             if symbol.type == lief.ELF.SYMBOL_TYPES.FUNC and symbol.value != 0:
                 relative_address = symbol.value - text_section.virtual_address
-                start_addrs.append(relative_address)
-                print("Function start @ " + str(relative_address))
+                start_addrs.append((relative_address, symbol.name))
+        
+        # create a new feature vector and set each address that is the start of a function as '1'
+        features = numpy.zeros(text_section.size, dtype=int)
+        
+        for addr, symbol_name in start_addrs:
+            if addr >= text_section.size:
+                print(f"[!] Warning: Address {addr} (from symbol {symbol_name}) is out of bounds for the .text section.")
+            else:
+                features[addr] = 1
+
+        return features
 
